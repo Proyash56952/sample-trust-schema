05f2d5ac1864e57ae09cc8c875d4a8e5afc36a00
Proyash Podder <proyash.podder@gmail.com>
2021-09-19 20:21:15 -0400

    Update ( A small error presents though)

--- TSP.py
+++ TSP.py
@@ -8,6 +8,7 @@ from collections import defaultdict
 
 idDict = {}
 defDict = defaultdict(list)
+cacheDict = defaultdict(list)
 #defDict = {}
 chainDict = {}
 pub = []
@@ -51,7 +52,9 @@ class CustomVisitor(dctVisitor):
             exp = ctx.expression().accept(self)
             constraints = ctx.constraints().accept(self) if ctx.constraints() else None
             certificates = ctx.certificates().accept(self) if ctx.certificates() else None
-            defDict[id].append([exp,constraints,certificates])
+            defDict[id.value].append(exp)
+            defDict[id.value].append(constraints)
+            defDict[id.value].append(certificates)
             
     def visitIdentifier(self, ctx:dctParser.IdentifierContext):
         id = Identifier()
@@ -104,7 +107,7 @@ class CustomVisitor(dctVisitor):
         return ctx.HASH().getText() + ctx.STRING().getText()
         
     def visitLiteral(self, ctx:dctParser.LiteralContext):
-        print(ctx.STRING().getText())
+        #print(ctx.STRING().getText())
         return ctx.STRING().getText()
     
     def visitFunction(self, ctx:dctParser.FunctionContext):
@@ -129,25 +132,111 @@ class CustomVisitor(dctVisitor):
             ids.append(c.accept(self))
         return ids
 
-#def translate(dict,file):
-    #with open (file, 'w') as output:
-        #for key,values in dict.items():
+
+def replace_identifier(exp,constraints):
+    res = []
+    if(constraints):
+        for m in constraints:
+            name = ''
+            for n in exp.value:
+                if n.value in idDict:
+                    name += idDict[n.value] + '/'
+                elif n.value in m:
+                    name += m[n.value] + '/'
+                else:
+                    name += n.value + '/'
+            res.append(name)
+    else:
+        name = ''
+        for n in exp.value:
+            if n.value in idDict:
+                name += idDict[n.value] + '/'
+            else:
+                name += n.value + '/'
+        res.append(name)
+    return res
+    
+#def replace_identifier(exp):
+    #name = []
+    #for e in exp.value:
+        #if e.value in idDict:
+            #name.append(idDict[e.value])
+        #else:
+            #name.append(e.value)
+    #return name
+    
+def replace_constraints(name,constraints):
+    res = []
+    for m in constraints:
+        temp = []
+        for n in name:
+            print(m,n)
+            #if(n in m):
+                #temp.append(m[n])
+                #print(m[n])
+            #else:
+                #temp.append(n)
+        res.append(temp)
+    return res
+    
+def add_cert(name, certificates):
+    res = []
+    for c in certificates:
+        for n in name:
+            r = n + '\t{' + c.value + '}'
+            res.append(r)
+    return res
             
+def translate(dict,file):
+    #print(defDict)
+    with open (file, 'w') as output:
+        for key,values in dict.items():
+            _exp = values[0]
+            _constraints = values[1]
+            _certificates = values[2]
+            
+            if(_exp.type == 'id'):
+                #if(_exp.value.value in cacheDict.keys()):
+                    #name = cacheDict[_exp.value.value]
+                #else:
+                previd = defDict.get(_exp.value.value)
+                #print(_exp.value.value)
+                #print("hola")
+                        #defDict[key][0] = previd[0]
+                        #defDict[key][1] = previd[1]
+                        #defDict[key][2] = previd[2]
+                _exp = previd[0]
+                        
+                if (not _constraints and previd[1]):
+                    _constraints = previd[1]
+                            
+                if (not _certificates and previd[2]):
+                    _certificates = previd[2]
+                    
+                    
+                #if(_exp.value in cacheDict.keys()):
+                    #print(cacheDict.get(key))
+                    #print("hola")
+            name = replace_identifier(_exp,_constraints)
+                
+            #if(_constraints):
+                #name = replace_constraints(name,_constraints)
+
+            
+            #print(key, name)
+            
+            #cacheDict[key].append(name)
+            #print(cacheDict[key])
+            if(_certificates):
+                name = add_cert(name,_certificates)
+            
+            for n in name:
+                print(key + ' : ' + n+'\n')
 
 def translate_signing_chain(file):
     with open (file, 'a') as output:
         output.write('\n\n\n\n')
         output.write('Signing Chain: \n\n')
-        #for key, value in chainDict.items():
-            #if (key in ['ocommand', 'ucommand']):
-                #i = key
-                #res = ''
-                #while i in chainDict.keys():
-                    #print(chainDict[i])
-                    #res += i + ' <= '
-                    #i = chainDict[i]
-                #res += i
-                #output.write(res + '\n\n')
                 
         for key, value in chainDict.items():
             i = key
@@ -176,15 +265,14 @@ if err == 0:
         tree.accept(visitor)
         print(idDict)
         #print(defDict)
-        for keys,values in defDict.items():
-            print(keys.value, values)
-            print('\n')
+        #for key,values in defDict.items():
+            
 
     except Exception as e:
         print("\nSyntax error occurred in the policy file!\n")
         sys.exit(1)
 
-    #translate(defDict, outputfile)
+    translate(defDict, outputfile)
     #print(chainDict)
     #print(cert)
     #translate_signing_chain(outputfile)
